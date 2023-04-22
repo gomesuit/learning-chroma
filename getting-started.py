ee666fae55347d197e81e08c0582bd3a169edff8
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-04-22 18:02:23 +0900

    save

--- getting-started.py
+++ getting-started.py
@@ -11,14 +11,16 @@ collection.add(
     ids=["id1", "id2"]
 )
 
-collection.add(
-    embeddings=[[1.2, 2.3, 4.5], [6.7, 8.2, 9.2]],
-    documents=["This is a document", "This is another document"],
-    metadatas=[{"source": "my_source"}, {"source": "my_source"}],
-    ids=["id1", "id2"]
-)
+# collection.add(
+#     embeddings=[[1.2, 2.3, 4.5], [6.7, 8.2, 9.2]],
+#     documents=["This is a document", "This is another document"],
+#     metadatas=[{"source": "my_source"}, {"source": "my_source"}],
+#     ids=["id1", "id2"]
+# )
 
 results = collection.query(
     query_texts=["This is a query document"],
     n_results=2
 )
+
+print(results)
