a29c0e2412b23ba7fe30b87659a468a43dada027
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-04-23 14:47:08 +0900

    removed some codes

--- samples/query.py
+++ samples/query.py
@@ -13,32 +13,13 @@ df = pd.read_csv(datafile_path)
 
 df["Id"] = df["Id"].astype(str)
 
-# print(df.head())
-
 sample_data = {}
-
-# for index, row in df.iterrows():
-#     sample_data[row["Id"]] = {
-#         "document": row["Text"],
-#         "metadata": {
-#             "product_id": row["ProductId"],
-#             "user_id": row["UserId"],
-#             "score": row["Score"],
-#         },
-#     }
 
 collection.add(
     documents=df["Text"].values.tolist(),
     metadatas=df[["ProductId", "UserId", "Score"]].to_dict(orient="records"),
     ids=df["Id"].values.tolist(),
 )
-
-# for id, data in sample_data.items():
-#     collection.add(
-#         documents=data["document"],
-#         metadatas=data["metadata"],
-#         ids=id
-#     )
 
 results = collection.query(
     query_texts=["This is a query document"],
